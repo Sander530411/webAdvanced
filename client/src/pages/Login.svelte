5fe374414d14dc576d87a74b2b8b411dd3d778f2
blocks1245 <meneersander@gmail.com>
2023-10-23 15:13:33 +0200

    aparently it works now, thx Jorn!

--- client/src/pages/Login.svelte
+++ client/src/pages/Login.svelte
@@ -1,42 +1,48 @@
 <script>
     let username = "";
     let password = "";
-
-    async function handleSubmit() {
-        try {
-            const response = await fetch("http://localhost:3000/auth", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ username, password }),
-            });
-
-            if (response.ok) {
-                // Successful login - Redirect or perform other actions
-                alert("Login successful!");
-            } else {
-                // Authentication failed
-                alert("Login failed. Please check your credentials.");
-            }
-        } catch (error) {
-            console.error("An error occurred:", error);
+  
+    async function handleSubmit(event) {
+      event.preventDefault();
+      try {
+        const response = await fetch("http://localhost:3000/auth", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
+        console.log(response);
+  
+        if (response.ok) {
+          // Successful login - Redirect or perform other actions
+          alert("Login successful!");
+          let token = await response.json();
+          console.log(token);
+          //TODO decode token
+          //TODO save token to a store
+          //TODO redirect to home page
+        } else {
+          // Authentication failed
+          alert("Login failed. Please check your credentials.");
         }
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
     }
-
+  
     export let params;
-
-</script>
-
-<main>
+  </script>
+  
+  <main>
     <h1>Login</h1>
     <form on:submit={handleSubmit}>
-        <label for="username">Username:</label>
-        <input type="text" id="username" bind:value={username} required>
-
-        <label for="password">Password:</label>
-        <input type="password" id="password" bind:value={password} required>
-
-        <button type="submit">Log In</button>
+      <label for="username">Username:</label>
+      <input type="text" id="username" bind:value={username} required />
+  
+      <label for="password">Password:</label>
+      <input type="password" id="password" bind:value={password} required />
+  
+      <button type="submit">Log In</button>
     </form>
-</main>
+  </main>
--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,30 +1,32 @@
-import express, {json} from 'express';
-import jsonwebtoken from 'jsonwebtoken';
+import express, { json } from "express";
+import jsonwebtoken from "jsonwebtoken";
 
 const router = express.Router();
 
-
 const users = [
-    { username: "admin", password: "admin" },
-    { username: "user", password: "user" }
+  { username: "admin", password: "admin" },
+  { username: "user", password: "user" },
 ];
 
 const secretKey = "secret-key-no-one-is-gonna-guess";
 
 router.post("/", async (req, res) => {
-    // @todo check the credentials and return an appropriate response
-    // For testing purposes a dummy token is returned.
-    let credentials = req.body;
+  // @todo check the credentials and return an appropriate response
+  // For testing purposes a dummy token is returned.
+  let credentials = req.body;
 
-    const user = users.find(u => u.username === credentials.username && u.password === credentials.password);
-    console.log(user);
-    if (user) {
-        // return web token
-        const token = jsonwebtoken.sign(payload, secretKey, { expiresIn: "1h" });
-        res.status(200).send({token});
-    } else {
-        res.status(401).send("Unauthorized");
-    }
+  const user = users.find(
+    (u) =>
+      u.username === credentials.username && u.password === credentials.password
+  );
+  console.log(user);
+  if (user) {
+    // return web token
+    const token = jsonwebtoken.sign(user, secretKey, { expiresIn: "1h" });
+    res.status(200).send({ token });
+  } else {
+    res.status(401).send("Unauthorized");
+  }
 });
 
 export default router;
